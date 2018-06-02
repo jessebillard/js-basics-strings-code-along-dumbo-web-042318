34850816253189b3cd198a62cabbe4ec34df10c0
jessebillard <jessebillard@mac.com>
2018-06-02 16:48:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+let currentUser = "jorge jimenez junior";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
